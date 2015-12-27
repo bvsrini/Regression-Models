29b8a100a3d3481bc27c1ba6792f0e5b41598f84
bvsrini <bvsrini@yahoo.com>
2015-12-27 13:22:29 -0600

    Commit message

---
+++ repo.bat
@@ -0,0 +1,6 @@
+echo # Regression-Models >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/bvsrini/Regression-Models.git
+git push -u origin master
